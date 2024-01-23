630e3655c9e4500cf645fae4076d55bbfcabbcec
FoxFil <filipp.vesker@gmail.com>
2024-01-23 15:34:10 +0300

    remove text print on video

--- main.py
+++ main.py
@@ -62,16 +62,6 @@ while 1:
                 keyboard.press("w")
             else:
                 keyboard.release("w")
-            cv2.putText(
-                frame,
-                str(round(right_EAR, 2)),
-                (50, 50),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                1,
-                (0, 255, 255),
-                2,
-                cv2.LINE_4,
-            )
 
         cv2.imshow("Video", frame)
         if cv2.waitKey(5) & 0xFF == ord("q"):
